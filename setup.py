c4b78769459ae512f7a938597f8d23859901ad27
Juan Rached <juanrached@gmail.com>
2024-11-12 15:26:02 -0500

    Delete CMakeLists.txt Add Files for RCLPY Migration

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
   
-d = generate_distutils_setup(
+package_name = 'behavior_selector2'  
+setup(
+    name=package_name,
+    version='0.0.0',
     packages=['rqt_pkg'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    install_requires=['setuptools'],
+    tests_require=['pytest'],
+    zip_safe=True.
+    maintainer='Brett',
+    maintainer_email='brett@todo.todo',
+    description='GUI for selecting global flight mode',
+    license='MIT',
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('resource/*')),
+        (os.path.join('share', package_name), ['plugin.xml']),
+        (os.path.join('lib', package_name), glob('scripts/*.py')),
+    ],
+    entry_points={
+        'console_scripts': [
+            'button_module = behavior_selector2.button_module:main',
+        ],
+    },
 )
 
-setup(**d)
